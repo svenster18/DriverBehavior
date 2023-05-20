d536ee8d9764034e181daf4a31c394ab487d2b35
rizki <mohamadrizki8@gmail.com>
2023-05-20 22:17:30 +0700

    fix detection class

--- app/src/main/java/com/androkit/driverbehavior/Detection.java
+++ app/src/main/java/com/androkit/driverbehavior/Detection.java
@@ -4,12 +4,20 @@ import com.google.firebase.database.IgnoreExtraProperties;
 
 @IgnoreExtraProperties
 public class Detection {
+    public int zigZag = 0;
+    public int sleepy = 0;
+    public int suddenBraking = 0;
+    public int suddenAcceleration = 0;
     public boolean normal = false;
 
     public Detection() {
     }
 
     public Detection(int zigZag, int sleepy, int suddenBraking, int suddenAcceleration) {
+        this.zigZag = zigZag;
+        this.sleepy = sleepy;
+        this.suddenBraking = suddenBraking;
+        this.suddenAcceleration = suddenAcceleration;
         if(zigZag == 0 && sleepy == 0  && suddenBraking == 0 && suddenAcceleration == 0) {
             normal = true;
         }
